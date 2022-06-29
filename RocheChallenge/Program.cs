bb22d4dcc0d803542973c41fe0702c19de1a089a
Arkadiusz Schwarz <arkadiusz.schwarz@pl.abb.com>
2022-06-29 09:46:01 +0200

    feat - Add initial REST API implementation

--- RocheChallenge/Program.cs
+++ RocheChallenge/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using RocheChallenge.DbContexts;
+using RocheChallenge.DTOs;
 
 Console.WriteLine("TODO WebAPI started");
 
@@ -39,6 +41,60 @@ app.UseSwaggerUI();
 #region MINIMAL API
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todos", async (TodoDbContext db) =>
+{
+    return await db.Todos.ToListAsync();
+});
+
+app.MapGet("/todos/{id}", async (TodoDbContext db, int id) =>
+{
+    return await db.Todos.FindAsync(id) switch
+    {
+        Todo todo => Results.Ok(todo),
+        null => Results.NotFound()
+    };
+});
+
+app.MapPost("/todos", async (TodoDbContext db, Todo todo) =>
+{
+    await db.Todos.AddAsync(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todos/{todo.Id}", todo);
+});
+
+app.MapPut("/todos/{id}", async (TodoDbContext db, int id, Todo todo) =>
+{
+    if (id != todo.Id)
+    {
+        return Results.BadRequest();
+    }
+
+    if (!await db.Todos.AnyAsync(x => x.Id == id))
+    {
+        return Results.NotFound();
+    }
+
+    db.Update(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.MapDelete("/todos/{id}", async (TodoDbContext db, int id) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Todos.Remove(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Ok();
+});
 
 #endregion
 
